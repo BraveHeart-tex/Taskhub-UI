dbb979ded20bfd7a6411be65efc3a631eb92363d
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2026-01-13 22:08:35 +0300

    refactor: migrate to Biome, update icon library, add Base UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-function App() {
+import { ModeToggle } from './components/mode-toggle';
+
+export function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="text-red-500 text-4xl font-bold tracking-tight">
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='text-red-500 text-4xl font-bold tracking-tight'>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
         natus?
       </div>
+      <ModeToggle />
     </div>
   );
 }
-
-export default App;
---
+++ src/shared/theme/theme-provider.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { type Theme, ThemeContext } from './theme-context';
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+  storageKey?: string;
+};
+
+export function ThemeProvider({
+  children,
+  defaultTheme = 'system',
+  storageKey = 'vite-ui-theme',
+}: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(() => {
+    if (typeof window === 'undefined') return defaultTheme;
+    return (localStorage.getItem(storageKey) as Theme) || defaultTheme;
+  });
+
+  useEffect(() => {
+    const root = document.documentElement;
+    root.classList.remove('light', 'dark');
+
+    if (theme === 'system') {
+      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
+        .matches
+        ? 'dark'
+        : 'light';
+
+      root.classList.add(systemTheme);
+    } else {
+      root.classList.add(theme);
+    }
+  }, [theme]);
+
+  const value = {
+    theme,
+    setTheme: (theme: Theme) => {
+      localStorage.setItem(storageKey, theme);
+      setTheme(theme);
+    },
+  };
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
