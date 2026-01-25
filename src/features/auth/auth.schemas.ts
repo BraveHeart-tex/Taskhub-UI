a04aac1b3219b50e34f7a56066ea4a7b6adac41b
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2026-01-25 20:36:33 +0300

    refactor(auth): extract password and username length constants

--- src/features/auth/auth.schemas.ts
+++ src/features/auth/auth.schemas.ts
@@ -1,16 +1,21 @@
 import z from 'zod';
+import {
+  MAX_USER_NAME_LENGTH,
+  MIN_USER_NAME_LENGTH,
+} from '../users/users.constants';
+import { MAX_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH } from './auth.constants';
 
 export const loginInputSchema = z.object({
   email: z.email(),
-  password: z.string().min(8).max(100),
+  password: z.string().min(MIN_PASSWORD_LENGTH).max(MAX_PASSWORD_LENGTH),
 });
 
 export type LoginInput = z.infer<typeof loginInputSchema>;
 
 export const signupInputSchema = z.object({
   email: z.email(),
-  password: z.string().min(8).max(100),
-  fullName: z.string().min(2).max(100),
+  password: z.string().min(MIN_PASSWORD_LENGTH).max(MAX_PASSWORD_LENGTH),
+  fullName: z.string().min(MIN_USER_NAME_LENGTH).max(MAX_USER_NAME_LENGTH),
 });
 
 export type SignupInput = z.infer<typeof signupInputSchema>;
@@ -19,7 +24,7 @@ export const userSchema = z.object({
   id: z.uuid(),
   email: z.email(),
   createdAt: z.iso.datetime(),
-  fullName: z.string().min(2).max(100),
+  fullName: z.string().min(MIN_USER_NAME_LENGTH).max(MAX_USER_NAME_LENGTH),
 });
 
 export type User = z.infer<typeof userSchema>;
