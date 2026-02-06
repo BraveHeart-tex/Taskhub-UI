310178e942ac6c81cb3474de1a8eb0053dd7e3b8
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2026-02-06 21:59:21 +0300

    refactor(app-layout): prevent unintended scrolling by tightening container overflow

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -6,7 +6,7 @@ import { UserMenu } from '@/components/user-menu';
 
 export function AppLayout() {
   return (
-    <div className='fixed inset-0 flex flex-col'>
+    <div className='fixed inset-0 flex flex-col overflow-hidden'>
       <header className='flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4'>
         <Link to='/boards' className='flex items-center gap-2 select-none'>
           <SquareKanbanIcon className='size-5' />
@@ -17,7 +17,7 @@ export function AppLayout() {
           <ModeSwitch />
         </div>
       </header>
-      <main className='flex-1 overflow-y-auto p-4'>
+      <main className='flex-1 min-h-0 p-4'>
         <Outlet />
       </main>
     </div>
